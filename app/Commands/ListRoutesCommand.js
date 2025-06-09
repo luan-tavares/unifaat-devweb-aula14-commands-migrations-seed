4b8978f603ef11df841528546c6c9cf976b27102
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2025-06-09 13:57:45 -0300

    routes

--- app/Commands/ListRoutesCommand.js
+++ app/Commands/ListRoutesCommand.js
@@ -1,6 +1,5 @@
 import Table from 'cli-table3';
-import express from 'express';
-import routes from "../../routes/routes.js";
+import ListRoutes from '../../Core/ListRoutes.js';
 
 export default {
     name: 'listar-rotas',
@@ -11,54 +10,11 @@ export default {
 
         const table = new Table({
             head: ['Método', 'Path'],
-            colWidths: [10, 45]
+            colWidths: [10, 60]
         });
 
-        const routeList = [];
+        const routeList = ListRoutes();
 
-        function extractRoutes(stack, prefix = '') {
-            stack.forEach((layer) => {
-                if (layer.route) {
-                    const { path, methods } = layer.route;
-                    const methodList = Object.keys(methods).map((m) => m.toUpperCase());
-
-                    methodList.forEach((method) => {
-                        routeList.push({
-                            method,
-                            path: prefix + path,
-                        });
-                    });
-                }
-
-                // Rota montada via router.use()
-                else if (layer.name === 'router' && layer.handle?.stack) {
-                    // tenta extrair o prefixo
-                    const nestedPrefix = getPathFromRegex(layer.regexp);
-                    extractRoutes(layer.handle.stack, prefix + nestedPrefix);
-                }
-            });
-        }
-
-        function getPathFromRegex(regexp) {
-            const match = regexp
-                .toString()
-                .replace('/^', '')
-                .replace('\\/?(?=\\/|$)/i', '')
-                .replace(/\\\//g, '/')
-                .replace(/\\\./g, '.')
-                .replace(/\$$/, '')
-                .replace(/\//g, '/');
-
-            return match === '^' ? '' : match;
-        }
-
-        const app = express();
-
-        app.use("/", routes);
-
-        console.log(app);
-
-        extractRoutes(app._router.stack);
         if (routeList.length === 0) {
             console.log('⚠️ Nenhuma rota registrada.');
         } else {
